e268e897895956a0513118580967fa5000976511
Зайцев Илья <ilyazaytsev2017@bk.ru>
2022-07-03 18:43:33 +0700

    Update aw.py

--- aw.py
+++ aw.py
@@ -1,4 +1,41 @@
-i = 0
-while i < 10:
-    print(i)
+from random import randint #Боже что это за говнокод
+
+print("Я загад 3-значное число.Попробуйте угадать какое именно.")
+print("Вот несколько подсказок:")
+print("Когда я говорю:         Я имею ввиду:")
+print("Pico                    Одна цифра верна,но находиться не на своей позиции")
+print("Fermi                   Одна цифра верна и находится на своей позиции")
+print("Bagels                  Не одна цифра не являеться правильной")
+print()
+print("У вас есть 10 попыток,чтобы угадачть заданное число!")
+
+a = randint(100, 999)   #генератор рандомного числа
+numberan = str(a)
+print(a)
+
+b = 0 
+while b < 10:
+    numbervd = input()
+    i = 0
+    while i < 3:
+        if int(numbervd[i]) == int(numberan[i]):
+            print("Fermi")
+        i +=1
+
+    c = 0
+    b = 0
+    while c < 3:
+        while b < 3:
+            if int(numbervd[c]) == int(numberan[b]):
+                print("Pico")
+            c +=1
+            b +=1
+
+   # f = 0
+   # while f < 3:
+   #     if int(numbervd[0]) != int(numberan[f]):
+   #         print("Bagels")
+   #     f +=1
+b +=1
     
+
